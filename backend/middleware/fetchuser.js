bb64ac9a5cee81eda3482804c214b186e12ce2cd
Abdullah Siddiqui <137316478+abdullahquantumx@users.noreply.github.com>
2024-03-27 18:06:14 +0530

    Update fetchuser.js

--- backend/middleware/fetchuser.js
+++ backend/middleware/fetchuser.js
@@ -10,7 +10,7 @@ const fetchuser = (req, res, next) => {
     try {
         const data = jwt.verify(token, JWT_SECRET);
         req.user = data.user;
-        next();
+        next();// Pass control to the next middleware or route handler
     } catch (error) {
         res.status(401).send({ error: "Please authenticate using a valid token" })
     }
